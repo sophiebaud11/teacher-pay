903f25e54381edd274d3fb889ba43419de4814c0
sophiebaud11 <38045114+sophiebaud11@users.noreply.github.com>
2021-03-12 14:41:22 -0500

    trying to get js to work lol

--- script.js
+++ script.js
@@ -1,117 +1,16 @@
-// require(["d3","geo","topo"],function(d3,geo,topojson){
-//     // you can now use topojson!
+// us = d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+//   if (error) throw error;
+// }).then(function(result) {
+//   console.log(us)
+//
 // });
-var margin = {
-    top: 10,
-    bottom: 10,
-    left: 10,
-    right:10
-};
-// console.log(d3.select("#map_viz"));
-var mapRatio = 0.5
-
-var width = 800 - margin.left - margin.right;
-
-var height = width * mapRatio;
-var active = d3.select(null);
-// console.log(height);
-
-var svg = d3.select("map.viz")
-    .attr("viewBox", [0, 0, 960, 500])
-    .attr('class', 'center-container')
-    .attr('height', height + margin.top + margin.bottom)
-    .attr('width', width + margin.left + margin.right);
-
-svg.append('rect')
-    .attr('class', 'background center-container')
-    .attr('height', height + margin.top + margin.bottom)
-    .attr('width', width + margin.left + margin.right)
-    .on('click', clicked);
-
-console.log(svg);
-
-// Promise.resolve(d3.json("./json/counties-10m.json"))
-//   .then(ready);
-Promise.resolve(d3.json('us-counties.topojson'))
-    .then(ready);
-console.log(d3.json('us-counties.topojson'));
-var projection = d3.geoAlbersUsa()
-    .translate([width /2 , height / 2])
-    .scale(width);
-
-console.log(topojson.feature(us, us.objects.counties).features);
-console.log(projection);
-// this is to make github pages load pls
-
-var path = d3.geoPath()
-    .projection(projection);
-
-var g = svg.append("g")
-    .attr('class', 'center-container center-items us-state')
-    .attr('transform', 'translate('+margin.left+','+margin.top+')')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom);
-//github please update
-function ready(us) {
-
-    g.append("g")
-        .attr("id", "counties")
-        .selectAll("path")
-        .data(topojson.feature(us, us.objects.counties).features)
-        .enter().append("path")
-        .attr("d", path)
-        .attr("class", "county-boundary")
-        .on("click", reset);
-
-    g.append("g")
-        .attr("id", "states")
-        .selectAll("path")
-        .data(topojson.feature(us, us.objects.states).features)
-        .enter().append("path")
-        .attr("d", path)
-        .attr("class", "state")
-        .on("click", clicked);
-
-
-    g.append("path")
-        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-        .attr("id", "state-borders")
-        .attr("d", path);
-
-};
-
-function clicked(d) {
-    if (d3.select('.background').node() === this) return reset();
-
-    if (active.node() === this) return reset();
-
-    active.classed("active", false);
-    active = d3.select(this).classed("active", true);
-
-    var bounds = path.bounds(d),
-        dx = bounds[1][0] - bounds[0][0],
-        dy = bounds[1][1] - bounds[0][1],
-        x = (bounds[0][0] + bounds[1][0]) / 2,
-        y = (bounds[0][1] + bounds[1][1]) / 2,
-        scale = .9 / Math.max(dx / width, dy / height),
-        translate = [width / 2 - scale * x, height / 2 - scale * y];
-
-    g.transition()
-        .duration(750)
-        .style("stroke-width", 1.5 / scale + "px")
-        .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
+async function getPromise() {
+    let result = await d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+      if (error) throw error;
+    });
+    console.log(result.objects)
+    return result.objects;
 }
-
-
-function reset() {
-    active.classed("active", false);
-    active = d3.select(null);
-
-    g.transition()
-        .delay(100)
-        .duration(750)
-        .style("stroke-width", "1.5px")
-        .attr('transform', 'translate('+margin.left+','+margin.top+')');
-
-}
-// change again pls
+console.log(getPromise())
+// console.log(us)
+// console.log(us.PromiseResult)
