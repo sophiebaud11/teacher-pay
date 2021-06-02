56bf4ee34e121dceff3752444719c05555270af7
sophiebaud11 <38045114+sophiebaud11@users.noreply.github.com>
2021-06-02 12:40:58 -0700

    fix last two graphs

--- script.js
+++ script.js
@@ -5,29 +5,12 @@ let us;
 d3.json("https://d3js.org/us-10m.v1.json")
   .then(function(data) {
     us = data;
-    // filter data here, then make 3 individual svgs
-    console.log(us)
+
     path = d3.geoPath();
     const width = 975;
     const height = 610;
     let isClicked;
-    // window.addEventListener("scroll", function(event) {
-    //   var top = this.scrollY;
-    //   console.log(top)
-    //   if (top > 3300 && top < 3500) {
-    //     clickOrReset()
-    //   }
-    // }, false);
 
-
-    // start automating the zooms
-    // var pageHeight = window.innerHeight;
-    //
-    // var scrollPercent = (pageHeight - window.scrollY)/pageHeight;
-    //
-    // if (scrollPercent > -3.9) {
-    //   clickOrReset()
-    // }
     const zoom = d3.zoom()
         .scaleExtent([1, 1])
         .on("zoom", zoomed);
@@ -37,26 +20,12 @@ d3.json("https://d3js.org/us-10m.v1.json")
         .attr("viewBox", [0, 0, width, height])
         .on("click", reset);
 
-    // svg.append("text")
-    //   .attr("x", width / 2 )
-    //   .attr("y", 20)
-    //   .style("font-size", "20")
-    //   .style("font-weight", "bold")
-    //   .style("font-family", "Poppins")
-    //   .style("text-anchor", "middle")
-    //   .text("U.S. Counties");
-    //
-
-//     console.log(topojson.feature(us, us.objects.states).features.filter(function(d) {
-//   return d.id == 29;
-// })
-
     const g = svg.append("g");
 
     var county_names = [{name: "Sullivan County, NY",
                          color: "#254E70"},
                         {name: "Duval County, FL",
-                         color: "#C6E0FF"},
+                         color: "#64113F"},
                         {name: "Platte County, NE",
                          color: "#00272B"}]
 
@@ -64,8 +33,6 @@ d3.json("https://d3js.org/us-10m.v1.json")
       .attr("id", "counties")
       .selectAll("path")
       .data(topojson.feature(us, us.objects.counties).features)
-      // .data(topojson.feature(us, us.objects.counties).features.filter(function(d) {
-      //   return d.id.startsWith("36") | d.id.startsWith("31") | d.id.startsWith("12");}))
       .join("path")
         .attr("d", path)
         .attr("cursor", "pointer")
@@ -74,17 +41,12 @@ d3.json("https://d3js.org/us-10m.v1.json")
         else if (d.id.startsWith("12")) {return "#f8d3e7"}
         else if (d.id.startsWith("31")) {return "#91b191"}
         else {return "#fff"}})
-        // .attr("stroke", function(d) {if (d.id.startsWith("36")) {return "#254E70"}
-        // else if (d.id.startsWith("12")) {return "#C6E0FF"}
-        // else if (d.id.startsWith("31")) {return "#00272B"}
-        // else {return "#d9d9d9"}})
         .style("fill", function(d) { if (d.id == "36105") {return "#254E70"}
         else if (d.id.startsWith("36")) {return "#C4D8F3"}
         else if (d.id == "12031") {return "#64113F"}
         else if (d.id.startsWith("12")) {return "#f8d3e7"}
         else if (d.id == "31141") { return "#00272B" }
         else if (d.id.startsWith("31")) {return "#91b191"}})
-        // .on("mouseover", function(d) { if (d.id == "36105" | d.id == "12031" | d.id == "31141") {d3.select(this).attr("fill", "#fff")}})
         .on("click", clickOrReset);
 
     const country_outline = g.append("g")
@@ -107,25 +69,6 @@ d3.json("https://d3js.org/us-10m.v1.json")
         .attr("z-index", function(d) {if (d.id == "36" | d.id == "12" | d.id == "31") { return "1" }
         else {return "0"}})
         .attr("stroke", "#cccccc")
-        // .attr("stroke", function(d) {if (d.id == "36") {return "#C4D8F3"}
-        // else if (d.id == "12") {return "#002e66"}
-        // else if (d.id == "31") { return "#91b191" }
-        // else {return "#cccccc"}})
-
-    // const ny_counties = g.append("g")
-    //   .attr("id", "counties")
-    //   .attr("cursor", "pointer")
-    //   .selectAll("path")
-    //   .data(topojson.feature(us, us.objects.counties).features.filter(function(d) {
-    //     return d.id.startsWith("12")}))
-    //   .join("path")
-    //     .attr("d", path)
-    //     .attr("class", "county-boundary")
-    //     .style("fill", function(d) { if (d.id == "36105") {return "#254E70"}
-    //     else if (d.id == "12031") {return "#C6E0FF"}
-    //     else if (d.id == "31141") { return "#00272B" }})
-    //     .on("click", clickOrReset);
-
     var size = 20
 
 
@@ -152,20 +95,6 @@ d3.json("https://d3js.org/us-10m.v1.json")
         .style("alignment-baseline", "middle")
         .style("font-size", "15")
         .style("font-family", "PT Sans")
-
-        // filter for particular states by FIPS code & make other counties not interactive
-      // us.objects.states.filter(function(d) { return d.geometries[0].id == stateID; })
-    //   // ids: NY,
-
-
-    // g.append("path")
-    //     .attr("id", "state-borders")
-    //     .attr("fill", "none")
-    //     .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)))
-    //     .attr("stroke", function(d) {if (d.id == "36" | d.id == "12" | d.id == "31") {return "#fff"}
-    //         else {return "#cccccc"}})
-    //     .attr("stroke-linejoin", "round");
-    //
     svg.call(zoom);
 
     function reset() {
@@ -198,7 +127,6 @@ d3.json("https://d3js.org/us-10m.v1.json")
       isClicked = 1;
     }
     function clickOrReset(event, d) {
-      // var img = document.getElementById("sullivan_info")
       console.log(isClicked)
       if (isClicked == 1) {
         reset()
@@ -228,7 +156,6 @@ d3.json("https://d3js.org/us-10m.v1.json")
         }
 
 
-        // img.style.display = "block";
 
       }
     }
